1a95da2c5c9f182cab8c5d0ca4787dc292997877
Bragatte <marcelobragatte@gmail.com>
2020-01-31 01:21:21 -0300

    Update posintall.sh

--- posintall.sh
+++ posintall.sh
@@ -166,6 +166,14 @@ EOF
 #Attention with paths, name of the images and version of python3.*#
 
 ### ------------------------------------------------------------------- ###
+sudo add-apt-repository ppa:gnome3-team/gnome3 -y
+sudo add-apt-repository ppa:vala-team -y
+sudo add-apt-repository ppa:gnome3-team/gnome3-staging -y
+sudo apt install cmake valac libgtk-3-dev libgee-0.8-dev libclutter-gtk-1.0-dev libclutter-1.0-dev libwebkit2gtk-4.0-dev libclutter-gst-3.0-dev
+git clone https://github.com/cheesecakeufo/komorebi.git
+cd komorebi
+mkdir build && cd build
+cmake .. && sudo make install && ./komorebi
 
 ### ---------------------------- After install ------------------------ ###
 ## Finishing, updating and cleaning##
