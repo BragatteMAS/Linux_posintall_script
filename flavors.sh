7a9963d11798c4febb21e0867d049361417cd8a5
Bragatte <marcelobragatte@gmail.com>
2020-08-19 04:34:19 -0300

    Add files via upload
    Install different flavors at your distro.

---
+++ flavors.sh
@@ -0,0 +1,13 @@
+
+### -------------------Alternative flavors for GNOME------------------- ###
+'''If want to change the flavor, delete de "#" before the command line'''
+#sudo apt install cinnamon-desktop-environment              #Cinnamon
+#sudo apt kubuntu-desktop                                   #KDE_Kubuntu
+#sudo apt install xubuntu-desktop                           #Xfce
+#sudo apt install ubuntu-mate-desktop                       #MATE
+#sudo apt install lubuntu-desktop                           #LXqt
+#sudo apt install ubuntu-budgie-desktop                     #Budgie
+
+#sudo add-apt-repository ppa:leaeasy/dde                    #UbuntuDDE (Deepin)
+#sudo apt update 
+#sudo apt install dde                                       #UbuntuDDE (Deepin)
