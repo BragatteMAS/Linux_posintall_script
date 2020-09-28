da889914775d04767c7fd525e0cc09a28a535acc
Bragatte <marcelobragatte@gmail.com>
2020-09-28 15:05:11 -0300

    Update spaces

--- auto_snap.sh
+++ auto_snap.sh
@@ -6,30 +6,30 @@ echo ' \n Auto install programs with SNAP \n '
 #Programs select from SNAP store <https://snapcraft.io/store>
 # -----------------------------VARIABLES-------------------------------- #
 SNAP_INSTALL=(
-bitwarden			#System|password manager
-code		 		#Terminal|best IDE
-audacity 			#Sound|audio editor and recording app
-spotify			    #Sound|digital music service
-photogimp	 		#Image|patch 'Adobe' for GIMP
-inkscape 			#Image|vector graphics software
-blender	 		    #Image|3D pipeline—model,animation,simulation,rendering
-gifcurry 			#Image|gif creator
+bitwarden		  	  #System|password manager
+code		 		      #Terminal|best IDE
+audacity 			    #Sound|audio editor and recording app
+spotify			      #Sound|digital music service
+photogimp	 		    #Image|patch 'Adobe' for GIMP
+inkscape 			    #Image|vector graphics software
+blender	 		      #Image|3D pipeline—model,animation,simulation,rendering
+gifcurry 			    #Image|gif creator
 obs-studio		    #Video|Edition
-kdenlive			#Video|Edition
-vlc				    #Video|Viewer
-arandr              #Video|monitor settings
-discord 			#Comunication|interaction/gamer chat
+kdenlive			    #Video|Edition
+vlc				        #Video|Viewer
+arandr            #Video|monitor settings
+discord 			    #Comunication|interaction/gamer chat
 telegram-desktop 	#Comunication|alternative for whatsapp
-rambox 			    #Comunication|join different systems of comunication
-opera			    #Productivity|browser
-todoist			    #Productivity|Task manager
-foliate			    #Productivity|ebook viewer
-feedreader			#Productivity|RSS
-zotero-snap			#Productivity|References
+rambox 			      #Comunication|join different systems of comunication
+opera			        #Productivity|browser
+todoist			      #Productivity|Task manager
+foliate			      #Productivity|ebook viewer
+feedreader			  #Productivity|RSS
+zotero-snap			  #Productivity|References
 homeserver		    #Productivity|Share folders in urls
-docker  			#Productivity|container environmental 'sudo groupadd docker' && 'sudo usermod -aG docker $USER'
-nextcloud-desktop   #Productivity|file synced
-pymol-oss           #Research
+docker  			    #Productivity|container environmental 'sudo groupadd docker' && 'sudo usermod -aG docker $USER'
+nextcloud-desktop #Productivity|file synced
+pymol-oss         #Research
 
 )
 # ---------------------------------------------------------------------- #
