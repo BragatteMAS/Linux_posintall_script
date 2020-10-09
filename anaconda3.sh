9607a72d53ff6ed9e0157fc604dce4590338fcec
Bragatte <marcelobragatte@gmail.com>
2020-10-09 04:22:45 -0300

    sources ok

--- anaconda3.sh
+++ anaconda3.sh
@@ -1,21 +1,21 @@
+###Auto install Anaconda 
 #!/usr/bin/env bash
-###Auto install anaconda 
 #Use SUDO user for this program!!!#
 ### ----------------------- Tools for Data Science -------------------- ###
 
 ##Anaconda##
-sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
+#sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
 
-wget -c https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O /home/$SUDO_USER/anaconda.sh #edit "Anaconda3-xxx" for updates
-bash /home/$SUDO_USER/anaconda.sh -b -u -p /home/$SUDO_USER/anaconda3
+#wget -c https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -O /home/$SUDO_USER/anaconda.sh #edit "Anaconda3-xxx" for updates
+#bash /home/$SUDO_USER/anaconda.sh -b -u -p /home/$SUDO_USER/anaconda3
 #-b(agree with license)-u(unsubscrive)-p(prefix home)
-sudo chmod -R 777 /home/$SUDO_USER/anaconda3
+#sudo chmod -R 777 /home/$SUDO_USER/anaconda3
 
-source /home/$LOGNAME/anaconda3/bin/activate ## activate base conda terminal
-source /home/$LOGNAME/.bashrc #activate installation
-conda config --set auto_activate_base true
-#to not activate : conda config --set auto_activate_base false
-source /home/$LOGNAME/anaconda3/bin/activate
+sudo -s source /home/$SUDO_USER/anaconda3/bin/activate ## activate base conda terminal
+sudo -s source /home/$SUDO_USER/.bashrc #activate installation
+#conda config --set auto_activate_base true
+#conda config --set auto_activate_base false #to not activate
+sudo -s source /home/$SUDO_USER/anaconda3/bin/activate
 
 #open new terminal and wirte "anaconda-navigator#
 #Create icon for launcher/desktop
